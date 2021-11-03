a9110e8d5d3ffec6edbe364e7b502cec148f8af0
Wagner G Jesus <wagner.jesus@esmenezes.com.br>
2021-11-03 11:08:26 -0300

    ajuste de cor e logo tipo

--- App.js
+++ App.js
@@ -2,17 +2,23 @@ import 'react-native-gesture-handler';
 enableScreens();
 
 import React from 'react';
+import {StatusBar, SafeAreaView, Dimensions} from 'react-native';
 import {enableScreens} from 'react-native-screens';
 import {Provider} from 'react-redux';
 
 import {store} from './src/redux/configureStore';
 import MainNavigator from './src/routes/MainNavigator';
-
+const screenHeight = Dimensions.get('screen').height;
 const App = () => {
   return (
-    <Provider store={store}>
-      <MainNavigator />
-    </Provider>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#073763" />
+      <SafeAreaView style={{flex: 1, height: 400}}>
+        <Provider store={store}>
+          <MainNavigator />
+        </Provider>
+      </SafeAreaView>
+    </>
   );
 };
 
--- src/routes/MainStack.js
+++ src/routes/MainStack.js
@@ -1,26 +1,39 @@
 import React from 'react';
+import {StyleSheet} from 'react-native';
 import {useDispatch} from 'react-redux';
 import {createStackNavigator} from '@react-navigation/stack';
-import {TouchableOpacity} from 'react-native';
+import {TouchableOpacity, Image} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {AppConfigActions} from '../redux/actions';
 import Home from '../screens/Home';
 import Users from '../screens/Users';
-
 const Stack = createStackNavigator();
-
+import Logo from '../assets/logo.png';
 const MainStack = ({navigation}) => {
   const dispatch = useDispatch();
 
   const options = {
     title: null,
+    cardStyle: {backgroundColor: 'white'},
+    headerStyle: {
+      backgroundColor: '#073763',
+    },
+    headerTintColor: '#f0f',
+    headerTitle: '',
+
     headerLeft: () => (
-      <TouchableOpacity onPress={navigation.openDrawer}>
-        <Icon
+      <TouchableOpacity
+        style={{
+          width: 256,
+          height: 59,
+        }}
+        onPress={navigation.openDrawer}>
+        <Image source={Logo} style={styles.logo} />
+        {/* <Icon
           name="view-headline"
           size={30}
           style={{color: '#000', marginRight: 10}}
-        />
+        /> */}
       </TouchableOpacity>
     ),
     headerRight: () => (
@@ -29,7 +42,7 @@ const MainStack = ({navigation}) => {
         <Icon
           name="view-headline"
           size={30}
-          style={{color: '#000', marginRight: 5}}
+          style={{color: '#fff', marginRight: 5}}
         />
       </TouchableOpacity>
     ),
@@ -42,4 +55,10 @@ const MainStack = ({navigation}) => {
     </Stack.Navigator>
   );
 };
+const styles = StyleSheet.create({
+  logo: {
+    width: '100%',
+    height: '100%',
+  },
+});
 export default MainStack;
